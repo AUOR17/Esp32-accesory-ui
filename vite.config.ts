ebad4730a2e7cdcf8379ff787b84777d2ede34a1
AUOR17 <angel_osorno@outlook.com>
2025-03-12 16:39:19 -0600

    Fix vite config for S3

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // <- Rutas relativas para S3
+  build: {
+    outDir: 'dist', // <- Carpeta de salida
+    assetsDir: '', // <- Evita que Vite cree una subcarpeta para archivos estáticos
+  }
 })
